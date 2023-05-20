d72c347a4fdd79037b96c790c4c75aa0108d592b
paulnotfromstrapi <134174455+paulnotfromstrapi@users.noreply.github.com>
2023-05-20 17:36:05 -0500

    Initial commit

---
+++ frontend/next.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '1337',
+        pathname: '/uploads/**',
+      },
+    ],
+  },
+}
+
+module.exports = nextConfig
